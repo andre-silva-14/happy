c4077890850ed38b34fcc9f6b0c719c261df8e9c
andre-silva-14 <silva.andre1997@gmail.com>
2020-10-13 21:50:35 +0200

    Add orphanages map
     - Orphanages map page and logic was added
     - Map features from MapBox with API Token.
     - Page styles were created.

---
+++ src/web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link, Switch } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css'
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Choose an orphanage on the map</h2>
+                    <p>Thousands of children are awaiting your visit :)</p>
+                </header>
+
+                <footer>
+                    <strong>Denver</strong>
+                    <span>Colorado</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[39.7350021,-104.9652965]}
+                zoom={11.4}
+                style={{ width: '100%', height: '100%'}}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff"/>
+            </Link>
+        </div>
+    );
+}
+
+export default OrphanagesMap;
