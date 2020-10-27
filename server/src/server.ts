668192cd16c76e31e5c2dafe37259f05e473694b
andre-silva-14 <silva.andre1997@gmail.com>
2020-10-27 17:05:55 +0100

    Restructure project Routes and Controllers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (_request, response) => {
-    return response.json({
-        message: "Hello World",
-    })
-});
+app.use(routes);
 
 app.listen(3333);
