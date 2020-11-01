b4c8eb0a39b99fec27a2fb4ad18e6f2fb9608fdb
andre-silva-14 <silva.andre1997@gmail.com>
2020-11-01 19:58:28 +0100

    Add Map Marker and Initialize new pages

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link, Switch } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Link } from 'react-router-dom';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+})
 
 function OrphanagesMap() {
     return (
@@ -35,9 +43,27 @@ function OrphanagesMap() {
                 <TileLayer 
                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+                <Marker
+                    position={[39.7350021,-104.9652965]}
+                    icon={mapIcon}
+                >
+                    <Popup
+                        closeButton={false}
+                        minHeight={240}
+                        maxWidth={240}
+                        className="map-popup"
+                    >
+                        Test
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                    
+                </Marker>
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff"/>
             </Link>
         </div>
