e81d396e0cf0ace13c40f4d32a6b5350174a153c
andre-silva-14 <silva.andre1997@gmail.com>
2020-11-02 15:19:24 +0100

    BugFix: Show Latitude and Longitude correctly

--- server/src/views/orphanages_view.ts
+++ server/src/views/orphanages_view.ts
@@ -5,8 +5,8 @@ export default {
     render(orphanage: Orphanage) {
         return {
             name: orphanage.id,
-            latitude: orphanage.id,
-            longitude: orphanage.id,
+            latitude: orphanage.latitude,
+            longitude: orphanage.longitude,
             about: orphanage.about,
             instructions: orphanage.instructions,
             opening_hours: orphanage.opening_hours,
